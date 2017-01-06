ca35bbe34a4d1f4b8338616c9a164aaa6a333a0c
r-plus <anasthasia.r@gmail.com>
2017-01-06 10:34:49 +0900

    Add support Carthage.

---
+++ HQPagerViewController/HQPagerViewControllerLibrary/HQPagerViewControllerLibrary.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for HQPagerViewControllerLibrary.
+FOUNDATION_EXPORT double HQPagerViewControllerLibraryVersionNumber;
+
+//! Project version string for HQPagerViewControllerLibrary.
+FOUNDATION_EXPORT const unsigned char HQPagerViewControllerLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HQPagerViewControllerLibrary/PublicHeader.h>
+
+
